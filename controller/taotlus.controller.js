a6808875f8f2f2cbf6feef560c9b56a1811c0a1d
heiki.kotsar@khk.ee <heiki5000@gmail.com>
2020-05-19 01:18:40 +0300

    'sdsds'

--- controller/taotlus.controller.js
+++ controller/taotlus.controller.js
@@ -221,7 +221,7 @@ exports.createUpdateCompany = async (req, res, next) => {
         const page = await browser.newPage();
         await page.goto(`https://tartukhk.herokuapp.com/pdf/${req.params.taotluseId}`, {waitUntil: 'networkidle0'});
         await page.pdf({
-            path: `praktikataotlused/${req.params.taotluseId}/${data[0].taotlus.opilase_nimi} ${data[0].taotlus.date}.pdf`, format: 'A4' 
+            path: `https://tartukhk.herokuapp.com/praktikataotlused/${req.params.taotluseId}/${data[0].taotlus.opilase_nimi} ${data[0].taotlus.date}.pdf`, format: 'A4' 
         });
         await browser.close();
 
